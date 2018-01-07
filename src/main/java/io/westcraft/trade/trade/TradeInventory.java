3d01d8c916ce91db86857876ca9dd9c596e723a4
AL_1 <allenmlantz@gmail.com>
2018-01-07 17:05:46 -0600

    Incorporated gui

--- src/main/java/io/westcraft/trade/trade/TradeInventory.java
+++ src/main/java/io/westcraft/trade/trade/TradeInventory.java
@@ -1,5 +1,6 @@
 package io.westcraft.trade.trade;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -8,8 +9,6 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
-import net.md_5.bungee.api.ChatColor;
 
 import java.util.List;
 
@@ -50,54 +49,59 @@ public class TradeInventory {
 
     public void build() {
         this.inventory = Bukkit.createInventory(player, 54, ""); // TODO: Insert title
-        // TODO: Insert items
-/*		ItemStack divider = new ItemStack(Material.STAINED_GLASS_PANE, 1, (byte) 15);
-		ItemStack acceptButton = new ItemStack(Material.EMERALD_BLOCK);
-		ItemStack declineButton = new ItemStack(Material.REDSTONE_BLOCK);
-		ItemStack oneDollar = new ItemStack(Material.GOLD_NUGGET);
-		ItemStack tenDollar = new ItemStack(Material.GOLD_INGOT);
-		ItemStack hundredDollar = new ItemStack(Material.DIAMOND);
-		ItemStack totalMoney = new ItemStack(Material.BUCKET);
-		
-		divider = setName(divider, ChatColor.BLACK + "Divider");
-		acceptButton = setName(acceptButton, ChatColor.GREEN + "Accept");
-		declineButton = setName(declineButton, ChatColor.RED + "Decline");
-		oneDollar = setName(oneDollar, ChatColor.WHITE + "Add $1");
-		tenDollar = setName(tenDollar, ChatColor.WHITE + "Add $10");
-		hundredDollar = setName(hundredDollar, ChatColor.WHITE + "Add $100");
-		totalMoney = setName(totalMoney, ChatColor.WHITE + "Total: " + "WORK IN PROGRESS");
-		
-		inventory.setItem(4, divider);
-		inventory.setItem(13, divider);
-		inventory.setItem(22, divider);
-		inventory.setItem(31, divider);
-		inventory.setItem(36, divider);
-		inventory.setItem(37, divider);
-		inventory.setItem(38, divider);
-		inventory.setItem(39, divider);
-		inventory.setItem(40, divider);
-		inventory.setItem(41, divider);
-		inventory.setItem(42, divider);
-		inventory.setItem(43, divider);
-		inventory.setItem(44, divider);
-		inventory.setItem(45, oneDollar);
-		inventory.setItem(46, tenDollar);
-		inventory.setItem(47, hundredDollar);
-		inventory.setItem(48, totalMoney);
-		inventory.setItem(49, divider);
-		inventory.setItem(50, acceptButton);
-		inventory.setItem(51, acceptButton);
-		inventory.setItem(52, declineButton);
-		inventory.setItem(53, declineButton);*/
+
+        ItemStack divider = new ItemStack(Material.STAINED_GLASS_PANE, 1, (byte) 15);
+        ItemStack acceptButton = new ItemStack(Material.EMERALD_BLOCK);
+        ItemStack declineButton = new ItemStack(Material.REDSTONE_BLOCK);
+        ItemStack oneDollar = new ItemStack(Material.GOLD_NUGGET);
+        ItemStack tenDollar = new ItemStack(Material.GOLD_INGOT);
+        ItemStack hundredDollar = new ItemStack(Material.DIAMOND);
+        ItemStack totalMoney;
+        if (moneyIn > 0) {
+            totalMoney = new ItemStack(Material.LAVA_BUCKET);
+        } else {
+            totalMoney = new ItemStack(Material.BUCKET);
+        }
+
+        divider = setItemName(divider, ChatColor.BLACK + "Divider");
+        acceptButton = setItemName(acceptButton, ChatColor.GREEN + "Accept");
+        declineButton = setItemName(declineButton, ChatColor.RED + "Decline");
+        oneDollar = setItemName(oneDollar, ChatColor.WHITE + "Add $1");
+        tenDollar = setItemName(tenDollar, ChatColor.WHITE + "Add $10");
+        hundredDollar = setItemName(hundredDollar, ChatColor.WHITE + "Add $100");
+        totalMoney = setItemName(totalMoney, ChatColor.WHITE + "Total: $" + moneyIn);
+
+        inventory.setItem(4, divider);
+        inventory.setItem(13, divider);
+        inventory.setItem(22, divider);
+        inventory.setItem(31, divider);
+        inventory.setItem(36, divider);
+        inventory.setItem(37, divider);
+        inventory.setItem(38, divider);
+        inventory.setItem(39, divider);
+        inventory.setItem(40, divider);
+        inventory.setItem(41, divider);
+        inventory.setItem(42, divider);
+        inventory.setItem(43, divider);
+        inventory.setItem(44, divider);
+        inventory.setItem(45, oneDollar);
+        inventory.setItem(46, tenDollar);
+        inventory.setItem(47, hundredDollar);
+        inventory.setItem(48, totalMoney);
+        inventory.setItem(49, divider);
+        inventory.setItem(50, acceptButton);
+        inventory.setItem(51, acceptButton);
+        inventory.setItem(52, declineButton);
+        inventory.setItem(53, declineButton);
     }
-    
-/*	public ItemStack setName(ItemStack is, String name){
+
+    private ItemStack setItemName(ItemStack is, String name) {
         ItemMeta m = is.getItemMeta();
         m.setDisplayName(name);
         m.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
         is.setItemMeta(m);
         return is;
-    }*/
+    }
 
     public void onInventoryClicked(InventoryClickEvent event) {
         // TODO: Add buttons for money and accept/decline, make sure that the player is not taking any items they are not supposed to be
