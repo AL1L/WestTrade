1f0a3fb8f89f8838b72d4d227f3e8b6531cb5877
AL_1 <allenmlantz@gmail.com>
2018-01-07 00:38:02 -0600

    Configure the command a bit more

--- src/main/java/io/westcraft/trade/commands/TradeCommand.java
+++ src/main/java/io/westcraft/trade/commands/TradeCommand.java
@@ -1,7 +1,10 @@
 package io.westcraft.trade.commands;
 
+import io.westcraft.trade.command.CommandSenders;
 import io.westcraft.trade.command.WestCmd;
 import io.westcraft.trade.command.WestCommand;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
@@ -9,11 +12,16 @@ import java.util.List;
 public class TradeCommand extends WestCommand {
     public TradeCommand(JavaPlugin plugin) {
         super(plugin, "trade", "Trade with players", "t");
+        setPermission("west.trade");
+        setMinArgs(1);
+        setMaxArgs(1);
+        setAllowedSenders(CommandSenders.PLAYER);
     }
 
     @Override
     public void onCommand(WestCmd cmd) {
-
+        Player p = cmd.getPlayerSender();
+        p.sendMessage(ChatColor.GOLD + "Well... this is awkward... I can't do anything quite yet :P");
     }
 
     @Override
